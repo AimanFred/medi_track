5b2a21f2ba1102db2ede0c9d864199be957ad180
AimanFred <irzanfarish@gmail.com>
2024-10-29 16:52:19 +0800

    feat: update deployed networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.24",
+    networks: {
+        cardona: {
+            url: "https://rpc.cardona.zkevm-rpc.com",
+            accounts: ["2e5676f83ad672518b53d624b372b2a37abb8ad4e16ecea9f46db34997049694"]
+        }
+    }
 };
